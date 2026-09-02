bd2622bde7af8e3de65708ff768717eb22683d3f
ABHISHEK LELLAPALLI <185485285+Abhisheklellapalli@users.noreply.github.com>
2026-09-02 14:18:49 +0530

    Add 4-bit ALU testbench in tb.v

---
+++ 4'b_ALU/In_verilog/tb.v
@@ -0,0 +1,23 @@
+// Code your testbench here
+// or browse Examples
+module alu_4bit_tb;
+  reg [3:0]a;
+  reg [3:0]b;
+  reg [2:0]opcode;
+  wire [3:0]result;
+  wire cout;
+  alu_4bit dut(.a(a),
+               .b(b),
+               .opcode(opcode),
+               .result(result),
+               .cout(cout));
+  initial begin
+    repeat(15) begin
+      a = $urandom_range(0,15); 
+      b = $urandom_range(0,15);
+      opcode = $urandom_range(0,7);
+      #1;
+      $display("opcode = %b(%d), a=%b(%d), b=%b(%d), Result=%b(%d), cout=%b(%d)",opcode,opcode,a,a,b,b,result,result,cout,cout);
+    end
+  end
+endmodule
