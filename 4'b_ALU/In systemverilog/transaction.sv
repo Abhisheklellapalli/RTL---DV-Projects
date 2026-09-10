234d134caf91b669ed0c504212fc26b36e54a1fb
ABHISHEK LELLAPALLI <185485285+Abhisheklellapalli@users.noreply.github.com>
2026-09-10 11:04:13 +0530

    Add result and carry-out to transaction class
    Added result and carry-out bits to the transaction class.

--- 4'b_ALU/In systemverilog/transaction.sv
+++ 4'b_ALU/In systemverilog/transaction.sv
@@ -3,6 +3,8 @@ class transaction;
   rand bit[3:0]a;
   rand bit[3:0]b;
   randc bit[2:0]opcode;
+  bit [3:0]result;
+  bit cout;
   function void display(string tag="");
     $display("%s opcode=%b a=%b b=%b ",tag,opcode,a,b);
   endfunction
